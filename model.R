b3ff9910a344921bccc43365613eb55226a13b25
Adriana Villamor <adriana.villamor@ices.dk>
2019-09-26 11:09:36 +0200

    Initial skeleton of FOs

--- model.R
+++ model.R
@@ -4,6 +4,18 @@
 ## After:
 
 library(icesTAF)
+require(dplyr)
 
 mkdir("model")
+
+#A. Trends by guild
+
+trends <- stock_trends(clean_sag)
+
+
+#B.Trends and current catches, landings and discards
+
+
+catch_trends <- CLD_trends(clean_sag)
+catch_current <- stockstatus_CLD_current(clean_sag)
 
