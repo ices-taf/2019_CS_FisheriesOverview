8ce03c2d13ed3fc109318984e2f023ca1cdefbc4
Adriana Villamor <adriana.villamor@ices.dk>
2019-12-09 14:30:32 +0100

    added EOs graphs

--- model.R
+++ model.R
@@ -21,8 +21,10 @@ clean_sag <- clean_sag %>% mutate(SSB = replace(SSB,StockKeyLabel == "lez.27.6b"
 
 clean_sag <- read.taf("data/clean_sag.csv")
 trends <- stock_trends(clean_sag)
+guild <- guild_trends(clean_sag)
 
 write.taf(trends, dir = "model")
+write.taf(guild, dir = "model")
 
 #B.Trends and current catches, landings and discards
 
